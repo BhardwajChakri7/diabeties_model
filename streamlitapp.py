e11836f6d575bc452dd876a112040c4bc91c68ab
BhardwajChakri7 <155712824+BhardwajChakri7@users.noreply.github.com>
2024-10-18 18:40:51 +0530

    Update and rename mdps_public.py to streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -5,7 +5,7 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabete_model.sav', 'rb'))
 # page title
 st.title('Diabetes Prediction using ML')
 
